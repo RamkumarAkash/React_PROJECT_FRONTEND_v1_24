85f3a40c8fce3b05c9ea356688cde90f261d9fae
Administrator <Administrator@192.168.3.135>
2024-03-12 12:29:31 +0000

    initial commit

---
+++ src/utils/display_configuration.js
@@ -0,0 +1,20 @@
+export const petViewConfig = {
+Details: [
+    {
+      key: "PetName",
+      value: "PetName",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "Breed",
+      value: "Breed",
+      type: "text",
+      editable: false,
+      required: true
+    },
+  ],
+Type: [
+  ],
+}
